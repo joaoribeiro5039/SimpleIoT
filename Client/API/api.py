68d5db84d65bd90666a65c6d7a701e6ff72eb78d
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-06-06 21:24:11 +0100

    Update API

--- Client/API/api.py
+++ Client/API/api.py
@@ -9,20 +9,23 @@ import pika
 global LineName
 global opc_client
 global rabbitmq_connection
-
-
-
-def data_change_handler(ocp_url : str, nodestart : str, rabbitmqserver : str):
-    
-    rabbitmq_channel.queue_declare(queue=node_id)
-    rabbitmq_channel.basic_publish(exchange='', routing_key=node_id, body=str(value))
-    
-    rabbitmq_connection.close()
-    print("New value:", value)
+global rabbitmq_channel
+def data_change_handler(node, value, data_type):
+    global rabbitmq_channel
+    rabbitmq_channel.queue_declare(queue=node)
+    rabbitmq_channel.basic_publish(exchange='', routing_key=node, body=str(value))
 
 
 
 app = FastAPI()
+
+
+#On Shutdown
+@app.on_event("startup")
+async def shutdown_event():
+    print("Starting API")
+    await asyncio.sleep(2)
+
 
 #Testing
 @app.get("/Testing")
@@ -43,9 +46,10 @@ def OPCServer(ocp_url : str):
 
 # Connect to this OPC Server
 @app.post("/StartClient")
-async def ConnectOPCServer(ocp_url : str, nodestart : str, rabbitmqserver : str,):
+async def ConnectOPCServer(ocp_url : str, rabbitmqserver : str,):
     global opc_client
     global rabbitmq_connection
+    global LineName
     if opc_client.isconnected():
         opc_client.disconnect()
 
@@ -55,33 +59,39 @@ async def ConnectOPCServer(ocp_url : str, nodestart : str, rabbitmqserver : str,
     objects = root.get_children()[0]
     nodes = objects.get_children()
     
+    global rabbitmq_connection
+    global rabbitmq_channel
     rabbitmq_connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmqserver))
     rabbitmq_channel = rabbitmq_connection.channel()
 
     for node in nodes:
         for subnode in node.get_children():
-            if nodestart in str(subnode):
+            if LineName in str(subnode):
                 node_id = str(subnode)
-                value = opc_client.get_node(node_id).get_value()
-                print(str(node_id) + " -> " + str(value))
-        
-    opc_client.disconnect()
-
-    asyncio.create_task(Monitor(ocp_url, nodestart, rabbitmqserver))
+                subscription = opc_client.create_subscription(100, data_change_handler)
+                monitored_item = subscription.subscribe_data_change(node_id)
+                subscription.start()
     return True
 
 # Get Filtered Values of the OPC UA Server
 @app.put("/StopClient")
 def opcuavalues():
-    global StopFlag
-    StopFlag = True
+    global opc_client
+    global rabbitmq_connection
+    rabbitmq_connection.close()
+    opc_client.disconnect()
     return {True}
+
+# Get Filtered Values of the OPC UA Server
+@app.put("/SetLine")
+def opcuavalues(line : str):
+    global LineName
+    LineName = line
+    return LineName
 
 #On Shutdown
 @app.on_event("shutdown")
 async def shutdown_event():
     print("Stopping pending Tasks")
-    global StopFlag
-    StopFlag = True
-    await asyncio.sleep(5)
+    await asyncio.sleep(2)
     print("Shutting down...")
