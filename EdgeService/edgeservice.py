ffe7305a6123b95f6a325e0b70da5ba84b29a622
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-06-28 02:30:28 +0100

    edge service on going

--- EdgeService/edgeservice.py
+++ EdgeService/edgeservice.py
@@ -7,48 +7,76 @@ import pika
 import json
 import datetime
 import threading
+from confluent_kafka import Producer
+import concurrent.futures
 
-global opc_clients
-opc_clients = []
+
+
+global machine_clients
+machine_clients = []
+
+global Kafka_Producer
+Kafka_Producer = []
+
+
+
 # create objects and variables from json file
 with open("monitorconfig.json", "r") as f:
     jsonservers = json.load(f)
 
 for server in jsonservers:
+
+    kafka_broker = "localhost:9092"
+    conf = {
+        'bootstrap.servers': kafka_broker,
+        'client.id': 'my_producer',
+        'acks': 'all'
+    }
+    producer = Producer(conf)
     opc_client = Client(server["url"])
     opc_client.connect()
     obj = {
         "opc_client": opc_client,
+        "kafka_producer": producer,
         "id": server["id"],
         "url": server["url"]
     }
-    opc_clients.append(obj)
+    machine_clients.append(obj)
 
-def Get_Nodes(opcClient):
-    print("Task Started for :" + str(opcClient["id"]) + "->" + opcClient["url"])
-    root = opcClient["opc_client"].get_root_node()
-    objects = root.get_children()[0]
-    nodes = objects.get_children()
-    for node in nodes:
-            for subnode in node.get_children():
-                node_id = str(subnode)
-                if "ns=1" in node_id:
-                    value = opcClient["opc_client"].get_node(node_id).get_value()
-                    timestamp = datetime.datetime.now()
-                    timestamp_str = timestamp.isoformat()
-                    message = {
-                            'Node': node_id,
-                            'TimeStamp': timestamp_str,
-                            'Value': str(value)
-                        }
-                    message_str = json.dumps(message)
+def Get_Nodes(opcClient, kafkaprod,kafkatopicprefix):
+    while True:
+        root = opcClient.get_root_node()
+        objects = root.get_children()[0]
+        nodes = objects.get_children()
+        for node in nodes:
+                for subnode in node.get_children():
+                    node_id = str(subnode)
+                    if "ns=1" in node_id:
+                        topic = kafkatopicprefix + "_" + node_id.split("=")[2].replace(".","_")
+                        value = opcClient.get_node(node_id).get_value()
+                        timestamp = datetime.datetime.now()
+                        timestamp_str = timestamp.isoformat()
+                        message = {
+                                'Node': node_id,
+                                'TimeStamp': timestamp_str,
+                                'Value': str(value)
+                            }
+                        message_str = json.dumps(message)
+                        kafkaprod.produce(topic, value=message_str)
+                        print(message_str)
 
-    print("Task Ended for :" + str(opcClient["id"]) + "->" + opcClient["url"])
+def process_machine_client(machine_client):
+    print(machine_client["url"])
+    Get_Nodes(machine_client["opc_client"], machine_client["kafka_producer"], str(machine_client["id"]))
 
+try:
+    time.sleep(2)
+    # Create a ThreadPoolExecutor with maximum worker threads
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_machine_client, machine_client) for machine_client in machine_clients]
+    concurrent.futures.wait(futures)
 
-for opc_client in opc_clients:
-    thread = threading.Thread(target=Get_Nodes(opc_client))
-    thread.start()
-
-for opc_client in opc_clients:
-    opc_client["opc_client"].disconnect()
+finally:
+    for machine_client in machine_clients:
+        machine_client["opc_client"].disconnect()
+        machine_client["kafka_producer"].flush()
