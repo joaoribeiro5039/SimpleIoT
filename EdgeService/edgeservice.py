ac521070cc0a041a2e239f24389ade0d51ab7673
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-06-30 19:56:04 +0100

    migrated to RabbitMQ

--- EdgeService/edgeservice.py
+++ EdgeService/edgeservice.py
@@ -5,7 +5,7 @@ from opcua import ua
 from typing import Optional
 import json
 import datetime
-from pika
+import pika
 import concurrent.futures
 
 
@@ -17,16 +17,18 @@ global Kafka_Producer
 Kafka_Producer = []
 
 
-def Get_Nodes(opcClient, kafkaprod,kafkatopicprefix):
+def Get_Nodes(opcClient, rabbitmq_connect,rabbitmq_queue):
+        
+        rabbitmq_channel = rabbitmq_connect.channel()
+        rabbitmq_channel.queue_declare(queue=rabbitmq_queue)
+
         root = opcClient.get_root_node()
         objects = root.get_children()[0]
         nodes = objects.get_children()
-        print(kafkatopicprefix)
         for node in nodes:
                 for subnode in node.get_children():
                     node_id = str(subnode)
                     if "ns=1" in node_id:
-                        topic = "Server" + kafkatopicprefix + "_" + node_id.split("=")[2].replace(".","_")
                         value = opcClient.get_node(node_id).get_value()
                         timestamp = datetime.datetime.now()
                         timestamp_str = timestamp.isoformat()
@@ -36,13 +38,11 @@ def Get_Nodes(opcClient, kafkaprod,kafkatopicprefix):
                                 'Value': str(value)
                             }
                         message_str = json.dumps(message)
-                        channel.basic_publish(exchange='', routing_key=queue_name, body=message)
-                        print(topic)
-        kafkaprod.flush()
+                        rabbitmq_channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=message_str)
 
 def process_machine_client(machine_client):
     print(machine_client["url"])
-    Get_Nodes(machine_client["opc_client"], machine_client["kafka_producer"], str(machine_client["id"]))
+    Get_Nodes(machine_client["opc_client"], machine_client["rabbitmq_connect"], str(machine_client["id"]))
 
 try:
     
@@ -51,35 +51,25 @@ try:
         jsonservers = json.load(f)
 
     for server in jsonservers:
-        # RabbitMQ connection parameters
-        connection_params = pika.ConnectionParameters('localhost')
-        
-        # Create a connection to the RabbitMQ broker
-        connection = pika.BlockingConnection(connection_params)
-        
-        # Create a channel
-        channel = connection.channel()
-        
-        # Declare a queue
-        queue_name = 'my_queue'
-        channel.queue_declare(queue=queue_name)
 
+        credentials = pika.PlainCredentials('admin', 'admin')
+        connection_params = pika.ConnectionParameters('cloud', credentials=credentials)
+        connection = pika.BlockingConnection(connection_params)
 
         opc_client = Client(server["url"])
         opc_client.connect()
         obj = {
             "opc_client": opc_client,
-            "kafka_producer": producer,
+            "rabbitmq_connect": connection,
             "id": server["id"],
             "url": server["url"]
         }
         machine_clients.append(obj)
 
     while True:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(machine_clients)) as executor:
             futures = [executor.submit(process_machine_client, machine_client) for machine_client in machine_clients]
 
 finally:
     for machine_client in machine_clients:
         machine_client["opc_client"].disconnect()
-        machine_client["kafka_producer"].flush()
