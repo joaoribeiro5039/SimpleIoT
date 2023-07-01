6a7d305aace04a8e30f25c8b5a1fccb046b121d0
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-07-01 17:34:30 +0100

    Updated OPC Server writing to kafka

--- SimpleOPCUAServer/opcuaserver.py
+++ SimpleOPCUAServer/opcuaserver.py
@@ -4,26 +4,49 @@ import random
 import datetime
 import json
 import numpy as np
+from confluent_kafka import Producer
 
 
 
-# create server
-server = Server()
-server.name = "SimpleOPCUA"
-server.set_endpoint("opc.tcp://0.0.0.0:4840")
+global opc_servers
+opc_servers = []
+
+global Kafka_Producer
+
+bootstrap_servers = 'cloud:9092'
+producer_config = {
+    'bootstrap.servers': bootstrap_servers,
+    'client.id': 'my-producer',
+    'acks': 'all'
+}
+Kafka_Producer = Producer(producer_config)
+
+
+for i in range(10):
+    server = Server()
+    server.name = "SimpleOPCUA"
+    endpoint = "opc.tcp://localhost:484" + str(i)
+    server.set_endpoint(endpoint)
+    obj = {
+        "opcserver" : server,
+        "id" : i
+    }
+    opc_servers.append(obj)
+    print(i)
+    print(endpoint)
 
 # create objects and variables from json file
 with open("nodes.json", "r") as f:
     jsonnodes = json.load(f)
 
-for node in jsonnodes:
-    obj = server.nodes.objects.add_object(node["node_id"], node["name"])
-    for var in node["variables"]:
-        opc_var = obj.add_variable(var["node_id"], var["name"], var["value"])
-        opc_var.set_writable(True)
+for server in opc_servers:
+    for node in jsonnodes:
+        obj = server["opcserver"].nodes.objects.add_object(node["node_id"], node["name"])
+        for var in node["variables"]:
+            opc_var = obj.add_variable(var["node_id"], var["name"], var["value"])
+            opc_var.set_writable(True)
+    server["opcserver"].start()
 
-# start server
-server.start()
 
 def get_Temperature_value(time, amplitude):
     frequency = 0.01
@@ -39,52 +62,39 @@ def get_Speed_value(time, amplitude):
         x = x * -1
     return x
 
+def UpdateServerValues(server, starttime):
+    end_time = time.time()
+    timeElapsed = round(end_time - starttime,6)
+    for i in range(4):
+        temperature = get_Temperature_value(timeElapsed,10.0 + i*10.0)
+        speed = get_Speed_value(timeElapsed,300.0 + i*100.0)
+        dt_time = datetime.datetime.now()
+        data = {
+            'Temperature': str(temperature),
+            'Speed': str(speed),
+            'DateTime': str(dt_time)
+            }
+        server["opcserver"].get_node("ns=1;s=Motor" + str(i+1) + ".DateTime").set_value(dt_time)
+        server["opcserver"].get_node("ns=1;s=Motor" + str(i+1) + ".Temperature").set_value(temperature)
+        server["opcserver"].get_node("ns=1;s=Motor" + str(i+1) + ".Speed").set_value(speed)
+
+        topic =  "Server_" + str(server["id"]) + "_Motor_" + str(i+1)
+        print(topic)
+        data_str = json.dumps(data)
+        Kafka_Producer.produce(topic, value=data_str)
 try:
     start_time = time.time()
     while True:
-        time.sleep(0.001)
-        end_time = time.time()
-        timeElapsed = round(end_time - start_time,6)
-        for node in jsonnodes:
-            for var in node["variables"]:
-                var_node = server.get_node(var["node_id"])
-                if "Motor1" in str(var_node):
-                    if "Temperature" in str(var_node):
-                        var_node.set_value(get_Temperature_value(timeElapsed,10.0))
-                    if "Speed" in str(var_node):
-                        var_node.set_value(get_Speed_value(timeElapsed,300.0))
-                    
-                if "Motor2" in str(var_node):
-                    if "Temperature" in str(var_node):
-                        var_node.set_value(get_Temperature_value(timeElapsed,20.0))
-                    if "Speed" in str(var_node):
-                        var_node.set_value(get_Speed_value(timeElapsed,400.0))
-                    
-                if "Motor3" in str(var_node):
-                    if "Temperature" in str(var_node):
-                        var_node.set_value(get_Temperature_value(timeElapsed,30.0))
-                    if "Speed" in str(var_node):
-                        var_node.set_value(get_Speed_value(timeElapsed,500.0))
-                    
-                if "Motor4" in str(var_node):
-                    if "Temperature" in str(var_node):
-                        var_node.set_value(get_Temperature_value(timeElapsed,40.0))
-                    if "Speed" in str(var_node):
-                        var_node.set_value(get_Speed_value(timeElapsed,600.0))
-                    
-                if "Motor5" in str(var_node):
-                    if "Temperature" in str(var_node):
-                        var_node.set_value(get_Temperature_value(timeElapsed,50.0))
-                    if "Speed" in str(var_node):
-                        var_node.set_value(get_Speed_value(timeElapsed,700.0))
-                
-                if "DateTime" in var["name"]:
-                    var_node.set_value(datetime.datetime.now())
-
-
+        start_time_Timer = time.time()
+        for server in opc_servers:
+            UpdateServerValues(server,start_time)
+        Kafka_Producer.flush()
+        end_time_Timer = time.time()
+        print(end_time_Timer - start_time_Timer)
 
 finally:
-    server.stop()
+    for server in opc_servers:
+        server["opcserver"].stop()
 
 
 
