aebfaf72a23bdd9d7f660acdad49a70c98ceb9a3
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-07-01 19:20:28 +0100

    set

--- DataService/dataservice.py
+++ DataService/dataservice.py
@@ -6,26 +6,29 @@ from cassandra.cluster import Cluster
 global rabbitmq_queues
 rabbitmq_queues = []
 for i in range(10):
-    rabbitmq_queues.append("Server_" + str(i) + "_Motor_1")
-    rabbitmq_queues.append("Server_" + str(i) + "_Motor_2")
-    rabbitmq_queues.append("Server_" + str(i) + "_Motor_3")
-    rabbitmq_queues.append("Server_" + str(i) + "_Motor_4")
-    rabbitmq_queues.append("Server_" + str(i) + "_Motor_5")
+    for imotor in range(1,6):
+        queue = "Server_" + str(i) + "_Motor_" + str(imotor)
+        credentials = pika.PlainCredentials('admin', 'admin')
+        connection_params = pika.ConnectionParameters('cloud', credentials=credentials)
+        connection = pika.BlockingConnection(connection_params)
+        rabbitmq_channel = connection.channel()
+        obj = {
+            "queue" : queue,
+            "channel" :rabbitmq_channel
+        }
+        rabbitmq_queues.append(obj)
 
 # Cassandra connection parameters
-cassandra_host = 'database'
-cassandra_keyspace = 'SimpleIoT'
+cassandra_host = "database"
+cassandra_keyspace = "simpleiot"
 
 # Connect to RabbitMQ
 
-credentials = pika.PlainCredentials('admin', 'admin')
-connection_params = pika.ConnectionParameters('cloud', credentials=credentials)
-connection = pika.BlockingConnection(connection_params)
-rabbitmq_channel = connection.channel()
 
 # Connect to Cassandra
 cluster = Cluster([cassandra_host])
 session = cluster.connect()
+session.execute(f"CREATE KEYSPACE IF NOT EXISTS {cassandra_keyspace} WITH replication = {{'class':'SimpleStrategy', 'replication_factor':3}}")
 session.set_keyspace(cassandra_keyspace)
 
 # Create tables in Cassandra if they don't exist
@@ -37,7 +40,7 @@ def process_message(channel, method, properties, body):
     table = method.routing_key
     # Write message to the corresponding table in Cassandra
     
-    # session.execute(f"INSERT INTO {table} (id, message) VALUES (uuid(), %s)", (body.decode(),))
+    session.execute(f"INSERT INTO {table} (id, message) VALUES (uuid(), %s)", (body.decode(),))
 
     # Acknowledge the message to remove it from the queue
     channel.basic_ack(delivery_tag=method.delivery_tag)
@@ -45,6 +48,6 @@ def process_message(channel, method, properties, body):
 # Start consuming messages from RabbitMQ queues
 
 for rabbitmq_queue in rabbitmq_queues:
-    rabbitmq_channel.basic_consume(queue=rabbitmq_queue, on_message_callback=process_message)
+    rabbitmq_queue["channel"].basic_consume(queue=rabbitmq_queue["rabbitmq_queue"], on_message_callback=process_message)
 
 rabbitmq_channel.start_consuming()
