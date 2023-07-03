a1c4e0c0184389d9df5df4c85d91b4cbccace7c0
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-07-04 00:14:39 +0100

    added prefix

--- rabbitmqtocassandra/rabbitmqtocassandra.py
+++ rabbitmqtocassandra/rabbitmqtocassandra.py
@@ -13,6 +13,12 @@ RabbitMQBroker = os.getenv("RABBITMQ_BROKER_HOST")
 if RabbitMQBroker is None:
     RabbitMQBroker = "rabbitmq"
 
+
+global RabbitMQ_Queue
+RabbitMQ_Queue = os.getenv("RABBITMQ_BROKER_QUEUE_PREFIX")
+if RabbitMQ_Queue is None:
+    RabbitMQ_Queue = "server1"
+
 global RabbitMQBroker_user
 RabbitMQBroker_user = os.getenv("RABBITMQ_BROKER_USER")
 if RabbitMQBroker_user is None:
@@ -34,12 +40,17 @@ TableName = os.getenv("CASSANDRA_DB_TABLENAME")
 if TableName is None:
     TableName = "Server1"
 
-global queue_list
 response = requests.get("http://" + RabbitMQBroker + ":15672/api/queues", auth=(RabbitMQBroker_user, RabbitMQBroker_password))
 if response.status_code == 200:
-    queue_list = [queue['name'] for queue in response.json()]
+    all_queue_list = [queue['name'] for queue in response.json()]
 else:
     print('Failed to retrieve queue list from RabbitMQ Management API.')
+
+global queue_list
+queue_list= []
+for queu in RabbitMQ_Queue:
+    if RabbitMQ_Queue in queu:
+        queue_list.append(queu)
 
 
 credentials = pika.PlainCredentials(RabbitMQBroker_user, RabbitMQBroker_password)
