d250684b657e737a5a740ee5c456d2c470dfb87f
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-07-06 00:38:59 +0100

    small update

--- rabbitmqtocassandra/rabbitmqtocassandra.py
+++ rabbitmqtocassandra/rabbitmqtocassandra.py
@@ -7,17 +7,18 @@ import concurrent.futures
 import uuid
 import os
 import requests
+import datetime
 
 global RabbitMQBroker
 RabbitMQBroker = os.getenv("RABBITMQ_BROKER_HOST")
 if RabbitMQBroker is None:
-    RabbitMQBroker = "rabbitmq"
+    RabbitMQBroker = "localhost"
 
 
 global RabbitMQ_Queue
 RabbitMQ_Queue = os.getenv("RABBITMQ_BROKER_QUEUE_PREFIX")
 if RabbitMQ_Queue is None:
-    RabbitMQ_Queue = "servera"
+    RabbitMQ_Queue = "server"
 
 global RabbitMQBroker_user
 RabbitMQBroker_user = os.getenv("RABBITMQ_BROKER_USER")
@@ -33,12 +34,19 @@ if RabbitMQBroker_password is None:
 global CassandraDB
 CassandraDB = os.getenv("CASSANDRA_DB_HOST")
 if CassandraDB is None:
-    CassandraDB = "cassandra"
+    CassandraDB = "localhost"
 
 global TableName
 TableName = os.getenv("CASSANDRA_DB_TABLENAME")
 if TableName is None:
     TableName = "Server1"
+
+global connection
+credentials = pika.PlainCredentials(RabbitMQBroker_user, RabbitMQBroker_password)
+connection_params = pika.ConnectionParameters(RabbitMQBroker, credentials=credentials)
+connection = pika.BlockingConnection(connection_params)
+global channel
+channel = connection.channel()
 
 global queue_list
 queue_list= []
@@ -53,10 +61,6 @@ else:
 
 
 
-credentials = pika.PlainCredentials(RabbitMQBroker_user, RabbitMQBroker_password)
-connection_params = pika.ConnectionParameters(RabbitMQBroker, credentials=credentials)
-connection = pika.BlockingConnection(connection_params)
-channel = connection.channel()
 
 cassandra_host = CassandraDB
 cassandra_keyspace = "simpleiot"
@@ -67,25 +71,31 @@ session = cluster.connect()
 session.execute(f"CREATE KEYSPACE IF NOT EXISTS {cassandra_keyspace} WITH replication = {{'class':'SimpleStrategy', 'replication_factor':3}}")
 session.set_keyspace(cassandra_keyspace)
 
-query = "CREATE TABLE IF NOT EXISTS {} (id UUID,nodeid TEXT, value TEXT,PRIMARY KEY (id))".format(TableName)
+query = "CREATE TABLE IF NOT EXISTS {} (id UUID,nodeid TEXT, value TEXT,opc_read_time TEXT,cassandra_write_time TEXT,PRIMARY KEY (id))".format(TableName)
 prepared_query = session.prepare(query)
 session.execute(prepared_query)
 
 def process_message(channel, method, properties, body):
     message = body.decode()
     _nodeid = method.routing_key
-    query = "INSERT INTO {} (id, nodeid, value) VALUES (?, ?, ?)".format(TableName)
+    query = "INSERT INTO {} (id, nodeid, value,opc_read_time,cassandra_write_time) VALUES (?, ?, ?, ?, ?)".format(TableName)
     prepared_query = session.prepare(query)
-    session.execute(prepared_query, (uuid.uuid4(), _nodeid, message))
-
-    channel.basic_ack(delivery_tag=method.delivery_tag)
+    msg_obj = json.loads(message)
+    msg_value = msg_obj['value']
+    msg_opcread = msg_obj['opc_read_time']
+    msg_time = str(datetime.datetime.now())
+    try:
+        session.execute(prepared_query, (uuid.uuid4(), _nodeid, msg_value ,msg_opcread, msg_time))
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    except:
+        print("Message not delivered")
 
 try:
-    
+
     for queue in queue_list:
         channel.basic_consume(queue=queue, on_message_callback=process_message)
-    print("Processing")
     channel.start_consuming()
+
 finally:
 
     print("Restarting")
