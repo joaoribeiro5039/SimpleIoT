ca6ba82593e1212d65477691904f31470a3d3d47
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-07-08 12:32:20 +0100

    Split in to background workers

--- opctobroker/opctobroker.py
+++ opctobroker/opctobroker.py
@@ -4,6 +4,7 @@ import random
 import datetime
 import numpy as np
 import pika
+import threading
 import os
 import json
 from confluent_kafka import Producer
@@ -44,7 +45,7 @@ else:
 
 #region Read Kafka Env Variables
 global usingKafka
-usingKafka =False
+usingKafka =True
 global KafkaBroker
 KafkaBroker = os.getenv("KAFKA_BROKER_HOST")
 if KafkaBroker is None:
@@ -73,49 +74,48 @@ OPC_Host = os.getenv("OPC_UA_HOST")
 if OPC_Host is None:
     OPC_Host = "localhost:4840"
 
-def browse_nodes_RabbitMQ(node):
+global stopflag
+stopflag = False
+def browse_nodes_RabbitMQ():
+    opc_client = Client( "opc.tcp://" + OPC_Host)
+    opc_client.connect()
+    node = opc_client.get_root_node()
     childrens = node.get_children()
-    for chlid in childrens:
-        nodes = chlid.get_children()
-        for node in nodes:
-            if "ns=1" in str(node):
-                for childnode in node.get_children():
-                    childnodeid = childnode.nodeid
-                    value = childnode.get_value()
-                    queue_name = RabbitMQ_Queue + "." + childnodeid.Identifier
-                    obj = {
-                        'value':str(value),
-                        'opc_read_time': str(datetime.datetime.now())
-                    }
-                    obj_json = json.dumps(obj)
-                    channel.queue_declare(queue=queue_name)
-                    channel.basic_publish(exchange='', routing_key=queue_name, body=obj_json)
+    
+    credentials = pika.PlainCredentials(RabbitMQBroker_user, RabbitMQBroker_password)
+    connection_params = pika.ConnectionParameters(RabbitMQBroker, credentials=credentials)
+    connection = pika.BlockingConnection(connection_params)
+    channel = connection.channel()
+    
+    try:
+        while True:
+            for chlid in childrens:
+                nodes = chlid.get_children()
+                for node in nodes:
+                    if "ns=1" in str(node):
+                        for childnode in node.get_children():
+                            childnodeid = childnode.nodeid
+                            value = childnode.get_value()
+                            queue_name = RabbitMQ_Queue + "." + childnodeid.Identifier
+                            obj = {
+                                'value':str(value),
+                                'opc_read_time': str(datetime.datetime.now())
+                            }
+                            obj_json = json.dumps(obj)
+                            channel.queue_declare(queue=queue_name)
+                            channel.basic_publish(exchange='', routing_key=queue_name, body=obj_json)
+    finally:
+        opc_client.disconnect()
+        connection.close()
+        stopflag = True
 
 
-def browse_nodes_Kafka(node):
+def browse_nodes_Kafka():
+    opc_client = Client( "opc.tcp://" + OPC_Host)
+    opc_client.connect()
+    node = opc_client.get_root_node()
     childrens = node.get_children()
-    for chlid in childrens:
-        nodes = chlid.get_children()
-        for node in nodes:
-            if "ns=1" in str(node):
-                for childnode in node.get_children():
-                    childnodeid = childnode.nodeid
-                    value = childnode.get_value()
-                    obj = {
-                        'value':str(value),
-                        'opc_read_time': str(datetime.datetime.now())
-                    }
-                    obj_json = json.dumps(obj)
-                    topic = KafkaProducerPref + "." + childnodeid.Identifier
-                    producer.produce(topic=topic, value=obj_json)
-
-if usingRabbit:
-        credentials = pika.PlainCredentials(RabbitMQBroker_user, RabbitMQBroker_password)
-        connection_params = pika.ConnectionParameters(RabbitMQBroker, credentials=credentials)
-        connection = pika.BlockingConnection(connection_params)
-        channel = connection.channel()
-
-if usingKafka:
+    
     bootstrap_servers = KafkaBroker
     producer_config = {
         'bootstrap.servers': bootstrap_servers,
@@ -125,34 +125,49 @@ if usingKafka:
 
     producer = Producer(producer_config)
 
-try:
-    # Connect to the OPC UA server
-    opc_client = Client( "opc.tcp://" + OPC_Host)
-    opc_client.connect()
-    
-    root = opc_client.get_root_node()
-    objects = root.get_children()
-    root_node = opc_client.get_root_node()
-    print(root_node)
-    while True:
-        start_time_Timer = time.time()
 
-        if usingRabbit:
-            browse_nodes_RabbitMQ(root_node)
-
-        if usingKafka:
-            browse_nodes_Kafka(root_node)
-
-        end_time_Timer = time.time()
-        print(end_time_Timer - start_time_Timer)
-
-finally:
-    # Disconnect from the OPC UA server
-    opc_client.disconnect()
-    
-    if usingRabbit:
-        connection.close()
-
-    if usingKafka:
+    try:
+        while True:
+            for chlid in childrens:
+                nodes = chlid.get_children()
+                for node in nodes:
+                    if "ns=1" in str(node):
+                        for childnode in node.get_children():
+                            childnodeid = childnode.nodeid
+                            value = childnode.get_value()
+                            obj = {
+                                'value':str(value),
+                                'opc_read_time': str(datetime.datetime.now())
+                            }
+                            obj_json = json.dumps(obj)
+                            topic = KafkaProducerPref + "." + childnodeid.Identifier
+                            producer.produce(topic=topic, value=obj_json)
+    finally:
+        opc_client.disconnect()
         producer.flush()
         producer.close()
+        stopflag = True
+
+if usingRabbit:
+    background_thread_Rabbit = threading.Thread(target=browse_nodes_RabbitMQ)
+    background_thread_Rabbit.daemon = True
+    background_thread_Rabbit.start()
+    
+if usingKafka:
+    background_thread_Kafka = threading.Thread(target=browse_nodes_Kafka)
+    background_thread_Kafka.daemon = True
+    background_thread_Kafka.start() 
+
+    
+
+while not stopflag:
+    print("Running")
+
+
+if usingRabbit:
+    background_thread_Rabbit.join()
+    
+if usingKafka:
+    background_thread_Kafka.join()
+
+print("Main program stopped")
